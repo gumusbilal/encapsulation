1668a903198ca91311960cfed78837e7f7fb85a0
Tipu <test@gmail.com>
2019-04-17 12:38:12 +0600

    first commit

---
+++ src/encapsulation/Encapsulation.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+import aladapackage.Tamanna;
+
+/**
+ *
+ * @author ADN
+ */
+public class Encapsulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Piyas ps = new Piyas();
+      
+      Jugol j = new Jugol();
+      
+        Tamanna t = new Tamanna();
+       
+      
+    }
+    
+}
---
+++ src/encapsulation/Jugol.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+/**
+ *
+ * @author ADN
+ */
+public class Jugol {
+    private String pen="Xiaomi";
+    protected String mobile="Huawei";
+    private int taka=5000;
+    
+    private void gf()
+    {
+        System.out.println(taka);
+    }
+    
+}
